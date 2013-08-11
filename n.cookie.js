519652f3886b679447d9d36cbf6682ac8787ebd7
王迎然 <ispxin@vip.qq.com>
2013-08-11 16:25:56 +0800

    Create n.cookie.js

---
+++ n.cookie.js
@@ -0,0 +1,55 @@
+/**
+ * @author www.wangyingran.com(王迎然)
+ * @module cookie模块
+ */
+!function(window){
+  
+	/**
+	 * Cookie模块参数说明
+	 * @name : cookie名
+	 * @value : cookie值
+	 * @time : cookie日期
+	 */
+	var Cookie = function(name, value, time) {
+		this.name = name;
+		this.value = value;
+		this.time = time;
+		this.oDate = new Date();
+	}
+	
+	Cookie.prototype = {
+		set : function() {
+			this.oDate.setDate(this.oDate.getDate() + this.time);
+	    	document.cookie = this.name + '=' + this.value + ';expires=' + this.oDate.toUTCString();
+		},
+		get : function() {
+			var arrA = document.cookie.split('; ');
+		    for (var i = 0; i < arrA.length; i++) {
+		        var arrB = arrA[i].split('=');
+		        if (arrB[0] == this.name) {
+		            return arrB[1];
+		            break;
+		        }
+		    }
+		}
+	}
+	
+	/**
+	 * 追加cookie模块到n命名空间上
+	 * @namespace n
+	 */
+	n.mix(n, {
+		cookie: function (name, value, time){
+			if (typeof value != 'undefined') {
+				if (value === null) {
+					new Cookie(name, '', -1).set();
+					return;
+				}
+				new Cookie(name, value, time).set();
+			} else {
+				return new Cookie(name).get();
+			}
+		}
+	});
+	
+}(this);
