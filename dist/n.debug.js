d5fdc6c523294c2234ff9ac592ae4170222d1d9d
johnqing <csssnow@gmail.com>
2013-08-11 21:50:43 +0800

    压缩合并

--- dist/n.debug.js
+++ dist/n.debug.js
@@ -572,52 +572,73 @@
  * @module cookie模块
  */
 !function(window){
-  
 	/**
-	 * Cookie模块参数说明
-	 * @name : cookie名
-	 * @value : cookie值
-	 * @time : cookie日期
+	 * cookie方法
+	 * @property {Object}
 	 */
-	var Cookie = function(name, value, time) {
-		this.name = name;
-		this.value = value;
-		this.time = time;
-		this.oDate = new Date();
-	}
-	
-	Cookie.prototype = {
-		set : function() {
-			this.oDate.setDate(this.oDate.getDate() + this.time);
-	    	document.cookie = this.name + '=' + this.value + ';expires=' + this.oDate.toUTCString();
+	var cookie = {
+		/**
+		 * 设置cookie
+		 * @param  {String} name    cookie名
+		 * @param  {String} value   cookie值
+		 * @param  {[type]} options 设置cookie的有效期，路径，域，安全  
+		 * @return 
+		 */
+		set: function(name, value, options){
+			var expires = '';     
+	        if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {     
+	            var date;     
+	            if (typeof options.expires == 'number') {     
+	                date = new Date();     
+	                date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));     
+	            } else {     
+	                date = options.expires;     
+	            }     
+	            expires = '; expires=' + date.toUTCString();     
+	        }     
+	        //设置参数
+	        var path = options.path ? '; path=' + (options.path) : '';     
+	        var domain = options.domain ? '; domain=' + (options.domain) : '';     
+	        var secure = options.secure ? '; secure': '';     
+	        document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join(''); 
 		},
-		get : function() {
-			var arrA = document.cookie.split('; ');
-		    for (var i = 0; i < arrA.length; i++) {
-		        var arrB = arrA[i].split('=');
-		        if (arrB[0] == this.name) {
-		            return arrB[1];
-		            break;
-		        }
-		    }
+		/**
+		 * 获取cookie
+		 * @param  {String} name cookie名
+		 * @return {Boolean}
+		 */
+		get: function(name){
+			var cookieValue = null;     
+			if (document.cookie && document.cookie != '') {     
+				var cookies = document.cookie.split(';');     
+				for (var i = 0; i < cookies.length; i++) {     
+				    var cookie = n.trim(cookies[i]);     
+				    if (cookie.substring(0, name.length + 1) == (name + '=')) {     
+				        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));     
+				        break;     
+				    }     
+				}     
+			}     
+			return cookieValue;
 		}
 	}
-	
 	/**
 	 * 追加cookie模块到n命名空间上
 	 * @namespace n
 	 */
 	n.mix(n, {
-		cookie: function (name, value, time){
-			if (!n.isUndefined(value)) {
-				if (n.isNull(value)) {
-					new Cookie(name, '', -1).set();
-					return;
-				}
-				new Cookie(name, value, time).set();
-			} else {
-				return new Cookie(name).get();
-			}
+		cookie: function (name, value, options){
+			if (!isUndefined(value)) {     
+		        options = options || {}; 
+		        //如果值为空，删除该cookie    
+		        if (isNull(value)) {     
+		            value = '';     
+		            options.expires = -1;     
+		        }  
+		        cookie.set(name, value, options);
+		    }else{
+				return cookie.get(name, value, options);
+		    }
 		}
 	});
 	
