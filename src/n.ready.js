9cc304099e6388b113ead1b679ca87d76d2238bd
王迎然 <ispxin@vip.qq.com>
2013-08-14 12:56:06 +0800

    Update n.ready.js
    修正bug，完美兼容~

--- src/n.ready.js
+++ src/n.ready.js
@@ -5,16 +5,16 @@
  */
 
 !function(window) {
-	
+
 	var document = window.document,
 		// 函数队列
 		funcQueue = [],
-		attEvent = document.attachEvent,
-		eventType = attEvent ? 'onreadystatechange' : 'DOMContentLoaded',
+		addEventListener = document.addEventListener,
+		eventType = addEventListener ? 'DOMContentLoaded' : 'readystatechange',
 		// 判断页面是否加载完毕
 		isReady = false,
 		readyBound = false;
-	
+
 	var fireReady = function() {
 		// 如果加载完毕，直接返回
 		if (isReady) return;
@@ -27,26 +27,32 @@
 		// 清空队列
 		funcQueue = null;
 	}
-	
+
 	var DOMContentLoaded = function() {
-		n.un(document, eventType, DOMContentLoaded);
-		fireReady();
-    	};
+		if (addEventListener) {
+			n.un(document, eventType, DOMContentLoaded);
+			fireReady();
+		} else if (document.readyState === "complete") {	// 却保包含iframe在ie6下完全加载
+			n.un(document, eventType, DOMContentLoaded);
+			fireReady();
+		}
+    };
 
 	var readyPromise = function() {
-		
+
 		if (readyBound) return;
 		readyBound = true;
-
+		
+		// 绑定事件
 		n.on(document, eventType, DOMContentLoaded);
-		// 如果是标准浏览器则使用 DOMContentLoaded
-		 if (attEvent){		
+
+		if (!addEventListener){
 			var top = false;
 
 			try {
 				top = window.frameElement == null && document.documentElement;
 			} catch(e) {}
-			
+
 			// 如果是ie，并且不存在iframe，连续检查是否加载完毕
 			if (top && top.doScroll) {
 				(function doScrollCheck() {
@@ -64,7 +70,7 @@
 		}
 
 	};
-	
+
 	var ready = function(f) {
 		if (isReady) {
 			f();
