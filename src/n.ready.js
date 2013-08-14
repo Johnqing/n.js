58d97cc986b7f9b0962fc8e303046123688750d6
王迎然 <ispxin@vip.qq.com>
2013-08-14 12:58:07 +0800

    Update n.ready.js

--- src/n.ready.js
+++ src/n.ready.js
@@ -32,7 +32,7 @@
 		if (addEventListener) {
 			n.un(document, eventType, DOMContentLoaded);
 			fireReady();
-		} else if (document.readyState === "complete") {	// 却保包含iframe在ie6下完全加载
+		} else if (document.readyState === "complete") {	// 确保包含iframe在ie6下完全加载
 			n.un(document, eventType, DOMContentLoaded);
 			fireReady();
 		}
