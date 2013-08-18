7336eaaf16e05be5458de2f6f2e149e125d521ea
johnqing <csssnow@gmail.com>
2013-08-18 22:55:02 +0800

    update: 添加数据缓存模块

--- src/n.data.js
+++ src/n.data.js
@@ -4,56 +4,88 @@
  * @link n.js
  */
 !function(n){
-	n._uuid = 2;
-	var nUid = n.nuid(),
-	nDate = {
-		getCacheIndex: function(el, isSetting){
-			if (n.isElement(el)) {
-				return !isSetting || nUid in el ? el[nUid] : (el[nUid] = ++n._uuid);
-			};
-			return n.isWindow(el) ? 0 : el.nodeType === 9 ? 1 : el.tagName === 'HTML' ? 2 : -1;
-		},
-		/**
-		 * 插入/获取缓存
-		 * @param  {Object}  el      当前dom元素
-		 * @param  {String}  type    命名空间: d(数据)
-		 * @param  {[type]}  key     [description]
-		 * @param  {[type]}  val     [description]
-		 * @param  {Boolean} isCache [description]
-		 * @return {[type]}          [description]
-		 */
-		data: function(el, type, key, val, isCache){
-			var cache = n.cache,
-				isValUn = n.isUndefined(val),
-				index = this.getCacheIndex(el, !isValUn);
-			if (n.isUndefined(index)) {
-				//不在缓存并且非undefined，创建一个缓存
-				if (!(index in cache) && !isValUn) {
-					cache[index] = {};
-				};
-				cache = cache[index];
-				//缓存如果为空 pass
-				if (!cache) {
-					return
-				};
-
-				
-			};
-		}
+	//生成时间戳
+	function timeStp(){
+		return (new Date).getTime();
 	}
-	n.mix(n.fn, {
-		data: function(name, val){
-			var _this = this;
-			if (n.isObject(name)) {
-				n.each(name, function(key, val){
-					_this.data(key, val);
-				});
-				return _this;
+	var exp = 'NJS' + timeStp(),
+		windowData = {};
+	n.exp = exp;
+	n.mix(n, {
+		isEmptyObject: function(obj){
+			for(var i in obj){
+				return false;
+			}
+			return true;
+		},
+		data: function(elem, name, data){
+			// 取得元素保存数据的键值
+			var nuid = elem[exp], 
+				cache = n.cache,
+				thisCache;
+			// 没有 id 的情况下，无法取值
+			if (!nuid && n.isString(name) && n.isUndefined(data)) {
+				return null;
 			};
+			//为元素计算一个唯一的id
+			if (!nuid) {
+				nuid = n.nuid();
+			};
+			//如果不在缓存 创建一个
+			if (!cache[nuid]) {
+				//在元素上保存键值
+				elem[exp] = nuid; 
+				//在 cache 上创建一个对象保存元素对应的值
+				cache[nuid] = {};
+			};
+			thisCache = cache[nuid];
+			//保存,防止重写
+			if (n.isUndefined(data)) {
+				thisCache[name] = data;
+			};
+			// 返回对应的值
+			return n.isString(name) ? thisCache[name] : thisCache;
 
-			if (n.isUndefined(val)) {
-				return nDate.data(_this[0], 'd', name);
-			};
+		},
+		removeData: function(elem, name){
+			// 取得元素保存数据的键值
+			var nuid = elem[exp], 
+				cache = n.cache,
+				thisCache = cache[nuid];
+
+			if (name) {
+				if (thisCache) {
+					//删除缓存
+					delete thisCache[name];
+					//元素上的缓存都被删掉的时候，删除缓存
+					if (n.isEmptyObject(thisCache)) {
+						n.removeData(elem);
+					};
+				};
+			}else{
+				delete elem[n.exp];
+				//删除全局缓存
+				delete cache[nuid];
+			}
 		}
-	});	
+	});
+	//
+	n.mix(n.fn, {
+		data: function(key, val){
+			//val不传，返回当前的key
+			if (n.isUndefined(val)) {
+				return n.data(this[0], key);
+			}else{
+				this.forEach(function(){
+					n.data(this, key, value);
+				});
+			}
+		},
+		removeData: function(){
+			return this.forEach(function(){
+				n.removeData(this, key);
+			});
+		}
+	});
+
 }(n);
